283f3c1d91d4629582f0fcb8ce3f5c941439a686
PBillingsby <philbillingsby@gmail.com>
2020-07-22 19:02:04 -0400

    Add innerHTML to baby-card

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,26 @@
-
 const BASEURL = 'http://localhost:3000/'
 document.addEventListener('DOMContentLoaded', () => {
-  fetchBaby()
+  event.preventDefault()
 })
+document.getElementById('baby-form').innerHTML = `
+  <div id="due_date_form">
+    <form class="p-3" onsubmit="newBaby();return false">
+      <div class="form-group">
+        <input type="text" name="mother_name" id="mother_name" placeholder="Mother Name">
+      </div>
+      <div class="form-group">
+        <input type="text" name="father_name" id="father_name" placeholder="Father Name">
+      </div>
+      <label>Due Date</label>
+  
+      <div class="form-group">
+        <input type="date" name="due_date" id="due_date">
+      </div>
+      <input type="submit" value="Add Baby">
+    </form>
+  </div>`
+
+}
 // BABY
 class Baby {
   constructor(due_date, mother, father, days_until_date, weeks_until_date) {
@@ -13,24 +31,8 @@ class Baby {
     this.weeks_until_date = weeks_until_date
   }
 }
-function fetchBaby() {
-  // event.preventDefault()
-  fetch(BASEURL + 'babies/1')
-  .then(resp => resp.json())
-  .then(babyObject => {
-    if (!babyObject.error) {
-      document.getElementById('due_date_form').style.display = "none"
-      let baby = new Baby(babyObject.due_date, babyObject.mother, babyObject.father, babyObject.days_until_date, babyObject.weeks_until_date)
-      handleBaby(baby)
-    }
-    else {
-      document.getElementById('due_date_form').style.display = "none"
-    }
-  })
-}
-
 function newBaby() {
-  event.preventDefault()
+
   let newBaby = {
     due_date: document.getElementById('due_date').value,
     mother: document.getElementById('mother_name').value,
@@ -44,19 +46,35 @@ function newBaby() {
     },
     body: JSON.stringify(newBaby)
   })
+  // REMOVES BABY FORM AND FETCHES CREATED BABY
   .then(resp => resp.json())
   .then(obj => {
-    document.getElementById('due_date_form').style.display = "none"
+    fetchBaby()
   })
 }
 
+function fetchBaby(e) {
+  fetch(BASEURL + 'babies/1')
+  .then(resp => resp.json())
+  // IF BABY EXISTS CREATE NEW BABY OBJECT AND SEND TO handleBaby() FUNCTION
+  .then(babyObject => {
+    if (!babyObject.error) {
+      let baby = new Baby(babyObject.due_date, babyObject.mother, babyObject.father, babyObject.days_until_date, babyObject.weeks_until_date)
+      handleBaby(baby)
+    }
+  })
+}
+
+
 function handleBaby(babyObject) {
-  document.getElementById('baby-information').innerHTML = `
-  <div class="card" style="width: 10rem;">
-    <p>Due Date: ${babyObject.due_date}</p>
-    <p>Mother: ${babyObject.mother}</p>
-    <p>Father: ${babyObject.father}</p>
-    <p>Days Until: ${babyObject.days_until_date}
+  // CREATE HTML FOR BABY INFORMATION DIV
+  document.getElementById('baby-card').innerHTML = `
+  <div class="card p-1" style="width: 10rem;">
+    <strong>Due Date:</strong> <p>${babyObject.due_date}</p>
+    <strong>Mother:</strong>  <p>${babyObject.mother}</p>
+    <strong>Father:</strong> <p>${babyObject.father}</p>
+    <strong>Days Until:</strong> <p>${babyObject.days_until_date}</p>
+    <strong>Weeks Until:</strong> <p>${babyObject.weeks_until_date}</p>
   </div>`
 }
 // APPOINTMENTS
