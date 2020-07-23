ec976db6f1e8436416769651cbcfeb70473d72dc
PBillingsby <philbillingsby@gmail.com>
2020-07-23 13:38:46 -0400

    Handle new appointment

--- backend/app/controllers/appointments_controller.rb
+++ backend/app/controllers/appointments_controller.rb
@@ -1,9 +1,15 @@
 class AppointmentsController < ApplicationController
   def create
-    byebug
+    appointment = Appointment.create(appointment_params)
+    render json: appointment
   end
 
-  def appointments_params
-    params.require(:appointment).permit(:doctor_name, :appointment_date, :location, :notes)
+  def index
+    appointments = Appointment.all
+    render json: appointments
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:baby_id, :doctor_name, :appointment_date, :location, :appointment_information)
   end
 end
