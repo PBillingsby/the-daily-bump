b844ee140e2ff5746c81b986d1fb88afbfc117d2
PBillingsby <philbillingsby@gmail.com>
2020-07-27 18:08:01 -0400

    Fix name_scraper to handle errors

--- backend/app/controllers/names_controller.rb
+++ backend/app/controllers/names_controller.rb
@@ -1,12 +1,19 @@
 class NamesController < ApplicationController
   def create
-    name = Name.create(baby_id: 1, name: params[:name])
-    byebug
+    name = Name.find_or_create_by(baby_id: 1, name: params[:name])
     render json: name
   end
 
   def index
     names = Name.all
+    if (names.count < 1)
+      names = {error: "No names yet"}
+    end
     render json: names
+  end
+
+  def show
+    name = Name.find_by(id: params[:id])
+    render json: name
   end
 end
--- backend/app/models/name.rb
+++ backend/app/models/name.rb
@@ -6,7 +6,9 @@ class Name < ActiveRecord::Base
   def name_scraper
     unparsed_url = HTTParty.get("https://www.behindthename.com/name/" + self.name)
     doc = Nokogiri::HTML(unparsed_url)
-    text = doc.css('section').first.children[3].text.gsub(/^\n/, "")
-    self.meaning = text.gsub('"', "")
+    if !doc.css('section').first.nil?
+      text = doc.css('section').first.children[3].gsub(/\R+/, '')
+      self.meaning = text.gsub('"', "")
+    end
   end
 end
