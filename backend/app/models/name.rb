f4f339449d72f3277c918aa8448c26606fd799dc
PBillingsby <philbillingsby@gmail.com>
2020-07-31 15:22:10 -0400

    Fix name scraper

--- backend/app/models/name.rb
+++ backend/app/models/name.rb
@@ -6,7 +6,9 @@ class Name < ActiveRecord::Base
   def name_scraper
     unparsed_url = HTTParty.get("https://www.behindthename.com/name/" + self.name)
     doc = Nokogiri::HTML(unparsed_url)
+    # FIX RETURN VALUE FROM SCRAPER
     if !doc.css('section').first.nil?
+       byebug
       text = doc.css('section').first.children[3].text.gsub(/\R+/, '')
       definition_text = doc.css('section')[4].children[3].text.gsub(/\R+/, '')
       self.meaning = text.gsub('"', "")
