ff94ae296f83c3a9fcc1bcf6d1d6cf5f7b0cd13f
PBillingsby <philbillingsby@gmail.com>
2020-08-01 15:13:24 -0400

    Fix name scraper to allow only descriptions

--- backend/app/models/name.rb
+++ backend/app/models/name.rb
@@ -6,13 +6,11 @@ class Name < ActiveRecord::Base
   def name_scraper
     unparsed_url = HTTParty.get("https://www.behindthename.com/name/" + self.name)
     doc = Nokogiri::HTML(unparsed_url)
-    # FIX RETURN VALUE FROM SCRAPER
+    # FIX RETURN VALUE FROM SCRAPER FOR DEFINITION
     if !doc.css('section').first.nil?
-       byebug
-      text = doc.css('section').first.children[3].text.gsub(/\R+/, '')
-      definition_text = doc.css('section')[4].children[3].text.gsub(/\R+/, '')
-      self.meaning = text.gsub('"', "")
-      self.definition = definition_text
+      name_definition = doc.xpath("//div[@style='text-align:justify']").text.gsub(/\R+/, '')
+      self.meaning = doc.css('section').first.children[3].text.gsub(/\R+/, '')
+      self.definition = name_definition.split(" ").map{|n| n.capitalize}.join(" ")
     else
       self.meaning = "No Meaning Found"
       self.definition = "No Definitions Found"
